209248668d1ea6ee39f93116f380467919be4c91
Yibo-Hao <sxyhao@hotmail.com>
2020-06-03 17:03:22 +0800

    导航样式修改

--- src/compontents/Layout.tsx
+++ src/compontents/Layout.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LayoutStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+const NavStyle = styled.nav`
+  line-height: 24px;
+  border-top: 0.5px solid rgba(0,0,0,0.25);
+  >ul{
+    display: flex;
+    >li{
+      width: 20%;
+      text-align: center;
+      padding: 8px;
+    }
+  }
+`
+const MainStyle = styled.div`
+  flex-grow: 1;
+  overflow:auto;
+`
 
 export default function Layout(props: { children: React.ReactNode; }) {
   return (
-    <div>
-      <div>
+    <LayoutStyle>
+      <MainStyle>
           {props.children}
-      </div>
-      <nav>
+      </MainStyle>
+      <NavStyle>
         <ul>
           <li>
             <Link to="/assets">资产</Link>
@@ -25,7 +48,7 @@ export default function Layout(props: { children: React.ReactNode; }) {
             <Link to="/more">更多</Link>
           </li>
         </ul>
-      </nav>
-    </div>
+      </NavStyle>
+    </LayoutStyle>
   );
 }
