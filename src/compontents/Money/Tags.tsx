b8066dad5b1cfbb03db339f94c970b12a99a8094
Yibo-Hao <sxyhao@hotmail.com>
2020-06-12 19:50:04 +0800

    tags 的功能

--- src/compontents/Money/Tags.tsx
+++ src/compontents/Money/Tags.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import styled from "styled-components";
 
 const TagsStyle = styled.section`
   padding: 10px 16px;
 
   ul {
-    border-top: 0.5px solid rgba(0,0,0,0.25);
+    border-top: 0.5px solid rgba(0, 0, 0, 0.25);
     display: flex;
     flex-direction: row;
     overflow-x: auto;
@@ -24,7 +24,11 @@ const TagsStyle = styled.section`
     }
   }
 `;
-export default function TagsMoney() {
+type Props = {
+  selected: string;
+  onChange: (tag: string) => void;
+};
+export default function TagsMoney(props: Props) {
   const [tags, setTags] = useState<string[]>([
     "服饰",
     "餐饮",
@@ -33,7 +37,7 @@ export default function TagsMoney() {
     "学习",
     "旅游"
   ]);
-  const [selectedTag, setSelectedTag] = useState<string[]>([]);
+  const [selectedTag, setSelectedTag] = useState<string>(props.selected);
   const onAddTag = () => {
     const name = window.prompt("请填写类型名");
     if (name && name.length <= 4) {
@@ -45,13 +49,14 @@ export default function TagsMoney() {
   const onToggleTag = (tag: string) => {
     const index = selectedTag.indexOf(tag);
     if (index >= 0) {
-      setSelectedTag([]);
+      setSelectedTag('');
     } else {
-      setSelectedTag([tag]);
+      setSelectedTag(tag);
     }
   };
   const getSelectedClass = (tag: string) =>
     selectedTag.indexOf(tag) >= 0 ? "selected" : "";
+  useEffect(()=>{props.onChange(selectedTag)},[selectedTag])
   return (
     <TagsStyle>
       <ul>
