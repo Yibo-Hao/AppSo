63220742ff1c71db46fd40799c6a5c1940fba365
Yibo-Hao <sxyhao@hotmail.com>
2020-06-13 14:52:17 +0800

    降低高度

--- src/compontents/Money/Back_Button.tsx
+++ src/compontents/Money/Back_Button.tsx
@@ -4,7 +4,7 @@ import Icon from "../Icon";
 const Button = styled.button`
   font-size: 22px;
   background: rgb(251, 251, 251);
-  padding: 15px 10px;
+  padding: 0 10px;
   :first-child {
     line-height: 30px;
   }
