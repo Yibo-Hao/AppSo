d972bd54991646f7f2599bf6e530892bf5bd0f13
Yibo-Hao <sxyhao@hotmail.com>
2020-06-18 22:16:48 +0800

    完成新增标签按钮的功能

--- src/compontents/Statistic/TagsStatistic.tsx
+++ src/compontents/Statistic/TagsStatistic.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useTags } from "../Money/useTags";
 import { Link, useRouteMatch } from "react-router-dom";
 import Icon from "../Icon";
+import { addCostId, addIncomeId } from "../Money/createid";
+import Modal from "../../lib/Modal";
 type TagsStyleProps = {
   height: string;
 };
@@ -19,13 +21,13 @@ const TagsStyle = styled.div<TagsStyleProps>`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background: rgba(157, 218, 185,0.65);
+    background: rgba(157, 218, 185, 0.65);
     border-radius: 50%;
     align-self: center;
     position: absolute;
     bottom: 10vh;
     right: 2vh;
-    >.content{
+    > .content {
       font-size: 10px;
       white-space: nowrap;
     }
@@ -72,9 +74,46 @@ const TagsStyle = styled.div<TagsStyleProps>`
     }
   }
 `;
+const DialogStyle = styled.div`
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.15);
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  .wrapper {
+    position: absolute;
+    top: 25%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    display: flex;
+    > .cost,
+    .income {
+      padding: 8px 16px;
+      background: white;
+      border-radius: 8px;
+      margin-left: 8px;
+    }
+  }
+`;
 const TagsStatistic: React.FunctionComponent<{ height: string }> = props => {
   const { costTags, setIncomeTags, setCostTags, incomeTags } = useTags();
+  const [modalState, setModalState] = useState(false);
+  const [dialogState, setDialogState] = useState(false);
+  const [type, setType] = useState("cost");
   const { url } = useRouteMatch();
+  const openDialogState = () => {
+    setDialogState(true);
+  };
+  const closeDialogState = () => {
+    setDialogState(false);
+  };
+  const openModalState = () => {
+    setModalState(true);
+  };
+  const closeModalState = () => {
+    setModalState(false);
+  };
   return (
     <TagsStyle height={props.height}>
       <ol className="cost">
@@ -97,8 +136,70 @@ const TagsStatistic: React.FunctionComponent<{ height: string }> = props => {
       </ol>
       <div className="button">
         <Icon name="add" />
-        <span className="content">新增</span>
+        <span className="content" onClick={openDialogState}>
+          新增
+        </span>
       </div>
+      {modalState ? (
+        <Modal
+          close={closeModalState}
+          empty={false}
+          initialValue=""
+          limit={5}
+          onChange={
+            type === "cost"
+              ? value => {
+                  for (let i = 0; i < costTags.length; i++) {
+                    if (costTags[i].name === value) {
+                      alert("请重新输入类别名");
+                      return;
+                    }
+                  }
+                  setCostTags([...costTags, { id: addCostId(), name: value }]);
+                }
+              : value => {
+                  for (let i = 0; i < incomeTags.length; i++) {
+                    if (incomeTags[i].name === value) {
+                      alert("请重新输入类别名");
+                      return;
+                    }
+                  }
+                  setIncomeTags([
+                    ...incomeTags,
+                    { id: addIncomeId(), name: value }
+                  ]);
+                }
+          }
+          placeholder="不能与已有类型名重复"
+          title="请输入类别名"
+        />
+      ) : null}
+      {dialogState ? (
+        <DialogStyle>
+          <div className="wrapper">
+            <div
+              className="cost"
+              onClick={() => {
+                setType("cost");
+                openModalState();
+                closeDialogState();
+              }}
+            >
+              支出
+            </div>
+            <div
+              className="income"
+              onClick={() => {
+                setType("income");
+                openModalState();
+                closeDialogState();
+              }}
+            >
+              收入
+            </div>
+          </div>
+        </DialogStyle>
+      ) : null}
     </TagsStyle>
   );
 };
