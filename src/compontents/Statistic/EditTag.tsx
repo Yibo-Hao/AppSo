455f39617bc75543ca527c025caa1e797d16cc0c
Yibo-Hao <sxyhao@hotmail.com>
2020-06-18 18:50:44 +0800

    editTag 组件wip

--- src/compontents/Statistic/EditTag.tsx
+++ src/compontents/Statistic/EditTag.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import { useTags } from "../Money/useTags";
 const EditTag: React.FunctionComponent = () => {
   const { id } = useParams();
-  console.log(id);
-  return <div>{id}</div>;
+  const { costTags, incomeTags, setIncomeTags, setCostTags } = useTags();
+  const findTag = (id: string) => {
+    let TagId = "";
+    if (id[0] === "c") {
+      TagId = id.slice(4);
+      return costTags.filter(tag => tag.id === Number(TagId))[0];
+    } else {
+      TagId = id.slice(6);
+     return incomeTags.filter(tag => tag.id === Number(TagId))[0];
+    }
+  };
+  return <div>{findTag(id).name}</div>;
 };
 export default EditTag;
