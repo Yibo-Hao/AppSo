2e7ea355811d9c633500ea83ebdbb34380de4743
Yibo-Hao <sxyhao@hotmail.com>
2020-06-19 19:34:01 +0800

    editTag 组件样式完成，功能wip

--- src/compontents/Statistic/EditTag.tsx
+++ src/compontents/Statistic/EditTag.tsx
@@ -1,6 +1,50 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useTags } from "../Money/useTags";
+import styled from "styled-components";
+import { addCostId, addIncomeId } from "../Money/createid";
+const EditTagStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  .nav {
+    width: 100%;
+    padding: 16px;
+    display: flex;
+    background: white;
+    justify-content: center;
+  }
+  .save {
+    margin: 0 auto;
+    padding: 8px 16px;
+    border-radius: 8px;
+    white-space: nowrap;
+    background: rgb(157, 218, 185);
+  }
+`;
+const InputWrapper = styled.section`
+  margin: 8px;
+  padding: 8px;
+  font-size: 16px;
+  background: white;
+  label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      vertical-align: bottom;
+    }
+    input {
+      line-height: 40px;
+      margin-left: 4px;
+      font-size: 16px;
+      vertical-align: bottom;
+      flex-grow: 1;
+      overflow: hidden;
+      color: black;
+      background: white;
+    }
+  }
+`;
 const EditTag: React.FunctionComponent = () => {
   const { id } = useParams();
   const { costTags, incomeTags, setIncomeTags, setCostTags } = useTags();
