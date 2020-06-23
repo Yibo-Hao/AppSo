3d1b6f7317ea24e4f90712f4d334d92851e8d109
Yibo-Hao <sxyhao@hotmail.com>
2020-06-23 13:52:30 +0800

    修复页面刷新id重新计数的bug

--- src/compontents/Money/createid.tsx
+++ src/compontents/Money/createid.tsx
@@ -1,14 +1,16 @@
 const createCostId = () => {
-  let costId = 4;
+  let costId = JSON.parse(window.localStorage.getItem("costId") || "4");
   return function CostId() {
     costId = costId += 1;
+    window.localStorage.setItem("costId", JSON.stringify(costId));
     return costId;
   };
 };
 const createIncomeId = () => {
-  let incomeId = 4;
+  let incomeId = JSON.parse(window.localStorage.getItem("incomeId") || "4");
   return function IncomeId() {
     incomeId = incomeId += 1;
+    window.localStorage.setItem("incomeId", JSON.stringify(incomeId));
     return incomeId;
   };
 };
