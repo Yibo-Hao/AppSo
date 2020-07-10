06cf8a72d99eeb42549f4e561c3e791fdaaf8ce9
Yibo-Hao <sxyhao@hotmail.com>
2020-07-10 16:16:49 +0800

    对时间进行分组排序

--- src/compontents/Statistic/timeLine.tsx
+++ src/compontents/Statistic/timeLine.tsx
@@ -3,75 +3,76 @@ import { useTags } from "../Money/useTags";
 import useRecord from "../Money/useRecord";
 import TimelineLine from "./timeline-line";
 import styled from "styled-components";
-const Box = styled.div<{ height: string; index: number[] }>`
-    overflow: auto;
-    height: ${props => props.height};
-    font-size: 16px;
+import RecordItem from "../Money/record.d";
+const Box = styled.div<{ height: string }>`
+  overflow: auto;
+  height: ${props => props.height};
+  font-size: 16px;
+  position: relative;
+  .timeline-item {
+    width: 100%;
+    margin-bottom: 40px;
+    padding: 0 5px;
+    &::after {
+      content: "";
+      display: block;
+      clear: both;
+    }
+  }
+  .timeline-img {
+    width: 15px;
+    height: 15px;
+    background: #3f51b5;
+    border-radius: 50%;
+    position: absolute;
+    left: 50%;
+    margin-top: 25px;
+    margin-left: -7.5px;
+  }
+  .timeline-content {
     position: relative;
-    .timeline-item {
-      width: 100%;
-      margin-bottom: 40px;
-      padding: 0 5px;
-      &::after {
-        content: "";
-        display: block;
-        clear: both;
-      }
-    }
-    .timeline-img {
-      width: 15px;
-      height: 15px;
-      background: #3f51b5;
-      border-radius: 50%;
-      position: absolute;
-      left: 50%;
-      margin-top: 25px;
-      margin-left: -7.5px;
-    }
-    .timeline-content {
-      position: relative;
-      width: 45%;
-      padding: 10px;
+    width: 45%;
+    padding: 10px;
+    border-radius: 4px;
+    background: rgb(190, 230, 208);
+    box-shadow: 0 20px 25px -15px rgba(0, 0, 0, 0.3);
+    .date {
+      background: #ff4081;
+      color: white;
+      padding: 3px;
       border-radius: 4px;
-      background: rgb(190, 230, 208);
-      box-shadow: 0 20px 25px -15px rgba(0, 0, 0, 0.3);
-      .date {
-        background: #ff4081;
-        color: white;
-        padding: 3px;
-        border-radius: 4px;
-        font-size: 12px;
-        position: absolute;
-        top: 0;
-        right: 0;
-      }
-      p{ 
-        margin-top: 5px;
-      text-indent:1em;
-        font-size: 14px;
-      }
-      .wrapper {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-      }
-      .title {
-        color: black;
-        font-size: 20px;
-      }
-      .output {
-        background: #3f51b5;
-        color: #ffffff;
-        padding: 8px 20px;
-        text-transform: uppercase;
-        display: inline-block;
-        border-radius: 4px;
-        margin-top: 5px;
-        font-size: 12px;
-        box-shadow: 0 1px 3px -1px rgba(0, 0, 0, 0.6);
-      }
+      font-size: 12px;
+      position: absolute;
+      top: 0;
+      right: 0;
     }
-  `;
+    p {
+      margin-top: 5px;
+      text-indent: 1em;
+      font-size: 14px;
+    }
+    .wrapper {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .title {
+      color: black;
+      font-size: 20px;
+    }
+    .output {
+      background: #3f51b5;
+      color: #ffffff;
+      padding: 8px 20px;
+      text-transform: uppercase;
+      display: inline-block;
+      border-radius: 4px;
+      margin-top: 5px;
+      font-size: 12px;
+      box-shadow: 0 1px 3px -1px rgba(0, 0, 0, 0.6);
+    }
+  }
+`;
 const Timeline: React.FunctionComponent<{ height: string }> = props => {
   const { records } = useRecord();
   const { getName } = useTags();
@@ -82,15 +83,6 @@ const Timeline: React.FunctionComponent<{ height: string }> = props => {
       (ref.current! as HTMLUListElement).getBoundingClientRect().height + "px";
     setHeight(ulHeight);
   }, []);
-  const findIndex = () => {
-    const indexArray: number[] = [];
-    for (let i = 0; i < records.length; i++) {
-      if (records[i].type === "+") {
-        indexArray.push(i);
-      }
-    }
-    return indexArray;
-  };
   const getStyle = (type: string) => {
     let style: { float: "right"; padding: string; background: string };
     if (type === "+") {
@@ -103,8 +95,22 @@ const Timeline: React.FunctionComponent<{ height: string }> = props => {
       return undefined;
     }
   };
+  const hash: { [key: string]: RecordItem[] } = {};
+  records.map(r => {
+    const key = r.date;
+    if (!(key in hash)) {
+      hash[key] = [];
+    }
+    hash[key].push(r);
+  });
+  const array = Object.entries(hash).sort((a, b) => {
+    if (a[0] === b[0]) return 0;
+    if (a[0] < b[0]) return 1;
+    if (a[0] > b[0]) return -1;
+    return 0
+  });
   return (
-    <Box height={props.height} index={findIndex()}>
+    <Box height={props.height}>
       <ul className="ul" ref={ref}>
         <TimelineLine height={height} />
         {records.map(r => {
@@ -117,7 +123,10 @@ const Timeline: React.FunctionComponent<{ height: string }> = props => {
                   <div className="date">{r.date}</div>
                 </div>
                 <p>{r.note}</p>
-                <div className="output">{r.type}{r.outPut}</div>
+                <div className="output">
+                  {r.type}
+                  {r.outPut}
+                </div>
               </div>
             </li>
           );
