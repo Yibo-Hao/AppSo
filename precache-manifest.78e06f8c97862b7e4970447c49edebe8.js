7e99bdf11df871d2c28eb0bbecc90f61715d40b4
Yibo-Hao <sxyhao@hotmail.com>
2020-08-05 22:15:16 +0800

    deploy

---
+++ precache-manifest.78e06f8c97862b7e4970447c49edebe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8dd14d4941aaeec94d5d3653fc607e9",
+    "url": "/AppSo/index.html"
+  },
+  {
+    "revision": "bd15b96d20baef3c3e8f",
+    "url": "/AppSo/static/css/main.b3f32332.chunk.css"
+  },
+  {
+    "revision": "cb4b2ce514d9d0393435",
+    "url": "/AppSo/static/js/2.8cb068ac.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/AppSo/static/js/2.8cb068ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd15b96d20baef3c3e8f",
+    "url": "/AppSo/static/js/main.45371ed4.chunk.js"
+  },
+  {
+    "revision": "4b4c2b0f91f17c1d737b",
+    "url": "/AppSo/static/js/runtime-main.fdad3dc5.js"
+  }
+]);
